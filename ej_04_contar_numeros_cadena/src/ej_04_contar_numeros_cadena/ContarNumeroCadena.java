a0135730b50f633eeb5fbd0e5de5eb5ed0a99d86
juanra02p <juanra02p@gmail.com>
2023-01-03 12:00:33 +0100

    empezados ejercicios

---
+++ ej_04_contar_numeros_cadena/src/ej_04_contar_numeros_cadena/ContarNumeroCadena.java
@@ -0,0 +1,14 @@
+package ej_04_contar_numeros_cadena;
+
+public class ContarNumeroCadena {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String cadena = "11,2,6,7,3,8,3,10,1,8,3";
+		
+		// Usa hashset 
+		
+		int numeros [] = cadena.split(",")
+	}
+
+}
