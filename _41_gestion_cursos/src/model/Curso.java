e28e07d7d460257f7851f042e9f5d7e4410c8e79
juanris1977 <120179191+juanris1977@users.noreply.github.com>
2023-01-18 12:35:11 +0100

    41 con maven

--- _41_gestion_cursos/src/model/Curso.java
+++ _41_gestion_cursos/src/model/Curso.java
@@ -1,56 +1,18 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Curso {
 	private String nombre;
 	private int duracion;
 	private double precio;
 	private String tematica;
 	
-	public Curso(String nombre, int duracion, double precio, String tematica) {
-		
-		this.nombre = nombre;
-		this.duracion = duracion;
-		this.precio = precio;
-		this.tematica = tematica;
-	}
-	
-	public Curso() {
-		
-	
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public int getDuracion() {
-		return duracion;
-	}
-
-	public void setDuracion(int duracion) {
-		this.duracion = duracion;
-	}
-
-	public double getPrecio() {
-		return precio;
-	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
-	}
-
-	public String getTematica() {
-		return tematica;
-	}
-
-	public void setTematica(String tematica) {
-		this.tematica = tematica;
-	} 
-	
-	
-
 }
